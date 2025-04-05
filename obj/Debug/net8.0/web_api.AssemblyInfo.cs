28ff8d13fab20f8b8741fd42c5f1ab060f5f4f52
Arabinda Krishna Kansakar <61239985+AvianaPhx@users.noreply.github.com>
2025-04-05 16:44:44 +0545

    test

--- obj/Debug/net8.0/web_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/web_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+749cec8bd6de25448a4be1cc6e78a91abc8a6187")]
 [assembly: System.Reflection.AssemblyProductAttribute("web_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
